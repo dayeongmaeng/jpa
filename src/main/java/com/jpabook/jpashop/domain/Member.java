0a7d1f5a87557e388ff3557efc57643cc68ac31d
Dayeong <dayeongmaeng@gmail.com>
2023-03-20 16:57:07 +0900

    feat: API 개발 기본

--- src/main/java/com/jpabook/jpashop/domain/Member.java
+++ src/main/java/com/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.domain;
 
+import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,13 +9,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded //내장 타입 포함
--- src/main/java/com/jpabook/jpashop/service/MemberService.java
+++ src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -17,11 +17,12 @@ public class MemberService {
 
     /**
      * 회원 가입
+     *
      * @param member
      * @return
      */
     @Transactional
-    public Long join(Member member){
+    public Long join(Member member) {
 
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
@@ -30,30 +31,39 @@ public class MemberService {
 
     /**
      * 중복 회원 검증
+     *
      * @param member
      */
     private void validateDuplicateMember(Member member) {
         //동시성 문제가 발생할 수도 있으니, DB에서 name에 유니크 제약 조건을 거는 것을 추천함
         List<Member> findMembers = memberRepository.findByName(member.getName());
-        if(!findMembers.isEmpty()){
+        if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
 
     /**
      * 회원 전체 조회
+     *
      * @return
      */
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
     /**
      * 회원 단건 조회
+     *
      * @param memberId
      * @return
      */
-    public Member findOne(Long memberId){
+    public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 }
